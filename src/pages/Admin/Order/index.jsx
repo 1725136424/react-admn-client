937cd16eb7c07dd8b2ec5475db4262d1a5423583
wanjiahao <1725136424@qq.com>
2021-04-15 23:54:58 +0800

    后台界面完成

--- src/pages/Admin/Order/index.jsx
+++ src/pages/Admin/Order/index.jsx
@@ -3,8 +3,14 @@ import React, {PureComponent} from 'react';
 class Order extends PureComponent {
     render() {
         return (
-            <div>
-                Order
+            <div style={{width: '100%',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: '30px',
+                color: "rgba(0, 0, 0, 0.65)"}}>
+                订单模块还在开发中...
             </div>
         );
     }
